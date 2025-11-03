e686f087ff2cb8764518d572ebe94999b70096f4
beni <193017@upf.br>
2025-11-03 19:27:50 -0300

    i think im doing too much...

--- main.cpp
+++ main.cpp
@@ -16,15 +16,27 @@ int main() {
 
     char command;
     vector<patient> arr,V,A,D,B;
-    int v,a,d,b,ate,peak,mins;
-    v=a=d=b=ate=peak=mins=0;
+    int v,a,d,b,ate,peak,mins,aV,aA,aD,aB;
+    v=a=d=b=ate=peak=mins=aV=aA=aD=aB=0;
     while(cin>>command) {
         patient temp;
         if (command == 'C') {
             cin >> temp.passkey >> temp.priority >> temp.hour >> temp.minute;
-            arr.push_back(temp);
+            if (temp.priority == 'V') {
+                V.push_back(temp);
+                v++;
+            } else if (temp.priority == 'A') {
+                A.push_back(temp);
+                a++;
+            } else if (temp.priority == 'D') {
+                D.push_back(temp);
+                d++;
+            } else if (temp.priority == 'B') {
+                B.push_back(temp);
+                b++;
+            }
         }
-        else if (command == 'A') {
+        else if (command == 'A') { // okay i gotta redo all this
             int temphr; int tempmin;
             cin >> temphr >> tempmin;
             for(auto i:arr) {
@@ -38,13 +50,13 @@ int main() {
         else if (command == 'Q') {
             cout << "--- RELATÓRIO FINAL ---" << endl
                  << "Total atendidos: " << ate << endl
-                 << "Por prioridade: V=" << v << " A=" << a << " D=" << d << " B=" << b << endl
+                 << "Por prioridade: V=" << aV << " A=" << aA << " D=" << aD << " B=" << aB << endl
                  << "Pico de lotação: " << peak << endl
                  << "Espera máxima: " << mins << " min" << endl;
             break;
         }
         else if (command == 'D') {
-            cout << "V:" << endl;// and the rest
+            cout << "V:" << v << " A:" << a << " D:" << d << " B:" << b << " | Atendidos: " << ate << endl;
         }
     }
 
